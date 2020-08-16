20666822dbf67ce13b242510b5ed772763e47b62
ducbaovn <ducbaovn@gmail.com>
2020-08-16 18:00:31 +0700

    Add animation

--- components/cube_tetris.js
+++ components/cube_tetris.js
@@ -4,17 +4,17 @@ import Cube from './cube.js';
 import PieceInCube from './piece_in_cube.js';
 
 export default class CubeTetris {
-  constructor(size, pieces) {
+  constructor(size, pieces, camera, scene, renderer) {
     this.size = size
     this.cube = new Cube(size);
     this.pieces = pieces;
     this.piecesInCube = pieces.map(piece => new PieceInCube(piece, this.cube))
-    this.solution = new MergePiece();
-    this.possible = pieces.reduce((count, p) => count += p.size, 0) === size * size * size
+    this.solution = new MergePiece(camera, scene, renderer);
+    this.canSolve = pieces.reduce((count, p) => count += p.size, 0) === size * size * size
   }
-  solve(x = 0, y = 0, z = 0) {
+  async solve(x = 0, y = 0, z = 0) {
     const targetPoint = new Point(x, y, z)
-    if (!this.possible || this.solution.merge().isEqual(this.cube)) return;
+    if (!this.canSolve || this.solution.merge().isEqual(this.cube)) return;
     if (this.solution.isContainsPoint(targetPoint)) {
       if (x === this.size - 1 && y === this.size - 1 && z === this.size - 1) return; 
       if (x === this.size - 1 && y === this.size - 1) return this.solve(0, 0, z + 1)
@@ -26,15 +26,15 @@ export default class CubeTetris {
       const pieceInCube = this.piecesInCube.shift()
       for (const piece of pieceInCube.getAll()) {
         if (piece.isContainsPoint(targetPoint) && this.solution.canMerge(piece)) {
-          this.solution.add(piece)
+          await this.solution.add(piece)
           if (x === this.size - 1 && y === this.size - 1) {
-            this.solve(0, 0, z + 1)
+            await this.solve(0, 0, z + 1)
           } else if (x === this.size - 1) {
-            this.solve(0, y + 1, z)
+            await this.solve(0, y + 1, z)
           } else {
-            this.solve(x + 1, y, z)
+            await this.solve(x + 1, y, z)
           }
-          if (!this.solution.merge().isEqual(this.cube)) this.solution.remove()
+          if (!this.solution.merge().isEqual(this.cube)) await this.solution.remove()
         }
       }
       if (!this.solution.merge().isEqual(this.cube)) this.piecesInCube.push(pieceInCube);
--- components/merge_piece.js
+++ components/merge_piece.js
@@ -1,8 +1,11 @@
 import Piece from "./piece.js";
 
 export default class MergePiece {
-  constructor() {
+  constructor(camera, scene, renderer) {
     this.pieces = [];
+    this.camera = camera;
+    this.scene = scene;
+    this.renderer = renderer;
   }
   isContainsPoint(point) {
     for (const piece of this.pieces) {
@@ -18,12 +21,22 @@ export default class MergePiece {
     }
     return true;
   }
-  add(piece) {
+  async add(piece) {
     if (!this.canMerge(piece)) throw Error("Cannot merge")
     this.pieces.push(piece);
+    if (this.camera != null && this.scene != null) {
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      this.scene.add(piece.toThree());
+      this.renderer.render(this.scene, this.camera);
+    }
   }
-  remove() {
+  async remove() {
     this.pieces.pop();
+    if (this.camera != null && this.scene != null) {
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      this.scene.children.pop();
+      this.renderer.render(this.scene, this.camera);
+    }
   }
   merge() {
     const points = [];
